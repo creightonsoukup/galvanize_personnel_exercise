b8da921c97e48c1d3132f68e20546a35782e44c5
Your Name <creighton@creightonsoukup.com>
2016-07-22 15:40:58 -0600

    got dynamic selector working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,17 @@
-// Your code here!
+$(document).ready(function() {
+    $.ajax({
+        url: 'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
+        error: function(err) {
+            console.error(err)
+        },
+        method: 'GET',
+        success: function(data) {
+            for (var i = 0; i < data.length; i++) {
+                option = data[i].title
+                $("#role").append("<option></option>")
+                $("option:nth-child(" + (i + 1) + ")").attr("id", "option" + i + "")
+                $("option:nth-child(" + (i + 1) + ")").text("" + data[i].title + "")
+            }
+        }
+    })
+})
