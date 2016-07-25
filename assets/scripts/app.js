4b024f1b59bb3f2997dacc835cd39a6813883863
Your Name <creighton@creightonsoukup.com>
2016-07-25 08:34:14 -0600

    worked on css

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
                 var option = data[i].title
                 images.push(data[i].img)
                 $("#role").append("<option></option>")
-                $("option:nth-child(" + (i + 1) + ")").attr("id", +i + "")
-                $("option:nth-child(" + (i + 1) + ")").text("" + data[i].title + "")
+                $("option:nth-child(" + (i + 2) + ")").attr("id", +i + "")
+                $("option:nth-child(" + (i + 2) + ")").text("" + data[i].title + "").css("font-family","'Roboto', sans-serif")
+
             }
+
             $("select").change(function() {
                 var image = $("select option:selected").attr("id")
                 $("img").attr("src", "" + images[image] + "")
@@ -31,10 +33,12 @@ $(document).ready(function() {
                     url: "http://galvanize-student-apis.herokuapp.com/gpersonnel/users",
                     data: formData,
                     error: function () {
-                      $(".save-status").text("Not quite.")
+                      $(".save-status").text("Not quite.").fadeIn(500).delay(2000).fadeOut(500).css("background-color","hsla(0, 82%, 66%, 0.51)")
                     },
                     success: function() {
-                        $(".save-status").text("Success!")
+                        $(".save-status").text("Success!").fadeIn(500).delay(2000).fadeOut(500).css("background-color","hsla(97, 76%, 63%, 0.51)")
+
+
                     }
                 })
             })
